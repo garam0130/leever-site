7803d245a791c529bf2de23d31db023540b16fbb
garam <garam>
2016-10-04 23:04:43 +0900

    work 수정

--- blog/static/js/main.js
+++ blog/static/js/main.js
@@ -241,3 +241,23 @@
 	});
 
 })(jQuery);
+
+
+
+
+
+//caption
+
+
+$(document).ready(function(){
+
+    $('#hover-cap-4col .thumbnail').hover(
+        function(){
+            $(this).find('.caption').slideDown(250); //.fadeIn(250)
+        },
+        function(){
+            $(this).find('.caption').slideUp(250); //.fadeOut(205)
+        }
+    );
+
+});
